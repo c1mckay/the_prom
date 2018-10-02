90c836f2476214fdd9ff88c36e1997744ff22286
vossnarrator <michaelcmkay@gmail.com>
2018-10-01 21:34:51 -0500

    random string

--- server_example.py
+++ server_example.py
@@ -1,7 +1,8 @@
 import time
 import random
+import string
 
-from prometheus_client import start_http_server, Counter, Summary
+from prometheus_client import start_http_server, Summary
 
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
@@ -14,11 +15,15 @@ def process_request(t):
     time.sleep(t)
 
 
+def random_string():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+
+
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
-    c = Counter('my_failurs', 'Description of counter')
-    c.inc(324)
+    Summary('my_summary', 'unused').observe(random_string())
+
     # Generate some requests.
     while True:
         process_request(random.random())
