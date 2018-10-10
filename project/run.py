fa4f9843cb20b91a1a77c92089a72b66a3a0741e
vossnarrator <michaelcmkay@gmail.com>
2018-10-10 00:33:23 -0500

    no more health check

--- project/run.py
+++ project/run.py
@@ -48,12 +48,12 @@ def add_health_check():
         contents = util.read_line('/sys/fs/lustre/health_check')
         return int(contents == 'healthy')
 
-    g = Gauge('health_checky', 'WHERE_IS_TEXT')
-    g.set_function(is_healthy)
-    g = Gauge('health_check', 'WHERE_IS_TEXT')
-    g.set_function(is_healthy)
-    g = Gauge('health_check', 'WHERE_IS_TEXT')
-    g.set_function(is_healthy)
+    # g = Gauge('health_checky', 'WHERE_IS_TEXT')
+    # g.set_function(is_healthy)
+    # g = Gauge('health_check', 'WHERE_IS_TEXT')
+    # g.set_function(is_healthy)
+    # g = Gauge('health_check', 'WHERE_IS_TEXT')
+    # g.set_function(is_healthy)
 
 
 def read_int_stat_func(url):
