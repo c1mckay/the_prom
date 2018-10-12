56f5627c0e37e538705cd96a6cc3c9dfc8406d63
Charles <cmckay@vacayhomeconnect.com>
2018-10-12 00:38:31 -0500

    sanity check

--- app/project/gauge_service.py
+++ app/project/gauge_service.py
@@ -2,6 +2,8 @@ from prometheus_client import Gauge
 
 
 def add_gauge(gauge_name, gauge_func, src=None):
-    g = Gauge(gauge_name, '', ['src'])
-    g.labels({'src': src}).set_function(gauge_func)
+    # g = Gauge(gauge_name, '', ['src'])
+    # g.labels({'src': src}).set_function(gauge_func)
+    g = Gauge(gauge_name, '')
+    g.set_function(gauge_func)
     return g
